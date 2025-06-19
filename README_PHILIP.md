b543e893289c6c0008cca105147e86176705421e
Philip Andrew Wee De Wang <41712275+PhilipWee@users.noreply.github.com>
2025-06-19 09:05:55 +0800

    Some random testing

---
+++ README_PHILIP.md
@@ -0,0 +1,4 @@
+# Build
+1. `cd build`
+2. `cmake -G "Visual Studio 17" -A x64 ..`
+3. `cmake --build .`
--- tools/buildtools/download_from_google_storage.py
+++ tools/buildtools/download_from_google_storage.py
@@ -3,7 +3,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 """Download files from Google Storage based on SHA1 sums."""
-
 from __future__ import print_function
 
 import hashlib
