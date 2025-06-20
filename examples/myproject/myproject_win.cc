f008b223caa1f4803307528a661b7b648e87e7c8
Philip Andrew Wee De Wang <41712275+PhilipWee@users.noreply.github.com>
2025-06-20 15:50:57 +0800

    Making some progress testing shit

--- examples/myproject/myproject_win.cc
+++ examples/myproject/myproject_win.cc
@@ -27,33 +27,12 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
                       int nCmdShow) {
   UNREFERENCED_PARAMETER(hPrevInstance);
   UNREFERENCED_PARAMETER(lpCmdLine);
+  // MessageBox(nullptr, L"Hello from wWinMain!", L"Info", MB_OK);
+  // OutputDebugStringW(L"My output string.");
 
-  int exit_code;
-
-#if defined(ARCH_CPU_32_BITS)
-  // Run the main thread on 32-bit Windows using a fiber with the preferred 4MiB
-  // stack size. This function must be called at the top of the executable entry
-  // point function (`main()` or `wWinMain()`). It is used in combination with
-  // the initial stack size of 0.5MiB configured via the `/STACK:0x80000` linker
-  // flag on executable targets. This saves significant memory on threads (like
-  // those in the Windows thread pool, and others) whose stack size can only be
-  // controlled via the linker flag.
-  exit_code = CefRunWinMainWithPreferredStackSize(wWinMain, hInstance,
-                                                  lpCmdLine, nCmdShow);
-  if (exit_code >= 0) {
-    // The fiber has completed so return here.
-    return exit_code;
-  }
-#endif
+  // int exit_code;
 
   void* sandbox_info = nullptr;
-
-#if defined(CEF_USE_SANDBOX)
-  // Manage the life span of the sandbox information object. This is necessary
-  // for sandbox support on Windows. See cef_sandbox_win.h for complete details.
-  CefScopedSandboxInfo scoped_sandbox;
-  sandbox_info = scoped_sandbox.sandbox_info();
-#endif
 
   // Provide CEF with command-line arguments.
   CefMainArgs main_args(hInstance);
@@ -61,11 +40,11 @@ int APIENTRY wWinMain(HINSTANCE hInstance,
   // CEF applications have multiple sub-processes (render, GPU, etc) that share
   // the same executable. This function checks the command-line and, if this is
   // a sub-process, executes the appropriate logic.
-  exit_code = CefExecuteProcess(main_args, nullptr, sandbox_info);
-  if (exit_code >= 0) {
-    // The sub-process has completed so return here.
-    return exit_code;
-  }
+  // exit_code = CefExecuteProcess(main_args, nullptr, sandbox_info);
+  // if (exit_code >= 0) {
+  //   // The sub-process has completed so return here.
+  //   return exit_code;
+  // }
 
   // Parse command-line arguments for use in this method.
   CefRefPtr<CefCommandLine> command_line = CefCommandLine::CreateCommandLine();
---
+++ examples/myproject/osr_handler.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "include/cef_render_handler.h"
+
+class OsrHandler : public CefRenderHandler {};
